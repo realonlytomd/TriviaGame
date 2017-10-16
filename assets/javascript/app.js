6cf91cfec6a5f582055c7796f57824aa210daca8
realonlytomd <mclaughlintomd@gmail.com>
2017-10-16 16:24:15 -0500

    update with html, css, beginning of jQ

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	console.log("hello");
+
+	// hide divs that are not shown at the beginning of game
+
+	//class="timeRemaining"
+	//class="questions"
+	//class="doneButton"
+	//class="allDone"
+
+	$(".timeRemaining").hide();
+	$(".questions").hide();
+	$("#doneButton").hide();
+	$(".allDone").hide();
+
+	//at the click of the start button,
+	// make the start button disappear
+	//make the time remaining div appear
+	//make the questions appear
+	//make the done button appear
+
+	$("#startButton").click(function(){
+
+		$("#startButton").hide();
+		$(".timeRemaining").show();
+		$(".questions").show();
+		$("#doneButton").show();
+
+	});
+
+	//jumping ahead:  when click the done button,
+	//make the done button disappear
+	//make time remaining div disappear
+	//make the questions div disappear
+	//make the allDone div appear
+
+	$("#doneButton").click(function(){
+
+		$("#doneButton").hide();
+		$(".timeRemaining").hide();
+		$(".questions").hide();
+		$(".allDone").show();
+
+	});
+
+	// when the questions come up, the countdown timer should start
+	
+
+
+
+});
